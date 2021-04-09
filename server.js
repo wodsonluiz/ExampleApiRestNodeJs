bc8b78a2eb4e4cd0ecc518c5ec81582fe0379f90
wodson <wodsonluiz@live.com>
2021-04-08 21:21:49 -0300

    begin crud

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+const bodyParser = require('body-parser');
+
 const app = require('./config/express')();
 const port = app.get('port');
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
 });
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.set('view engine', 'ejs')
+app.get('/', (req, res) =>{
+    res.render('index.ejs')
+})
+
+app.post('/show', (req, res) => {
+    console.log('Hellor again...')
+})
